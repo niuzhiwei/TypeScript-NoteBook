ed85f4d8fc7620916fccc37455aac60421a1cc3b
niuzhiwei <xujin02@qq.com>
2021-03-10 10:00:36 +0800

    原始数据类型

---
+++ basic-types.ts
@@ -0,0 +1,12 @@
+let isDone: boolean = false;
+let age: number = 10;
+let firstName: string = 'niuniu'
+let message: string = `Hello,${firstName}`
+let u: undefined = undefined;
+let n: null = null
+
+let num: number = undefined
+
+let notSure: any = 4
+notSure = 'maybe a string'
+notSure = true
